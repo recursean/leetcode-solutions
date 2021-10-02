d3d7e5bd60033bd39ccb3d0d299861c9a5ad5591
seanmcshane <seanmcshane96@gmail.com>
2021-10-02 17:28:30 -0400

    unfinished problem 22

---
+++ javacode/Pr22.java
@@ -0,0 +1,11 @@
+class Pr22 {
+    public List<String> generateParenthesis(int n) {
+        
+    }
+
+    public static void main(String[] args) {
+        Pr22 pr = new Pr22();
+        int n = 3;
+        pr.generateParenthesis(n);
+    }    
+}
