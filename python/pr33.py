5b548384f99ad456139321829a3b6168f40974b7
seanmcshane <seanmcshane96@gmail.com>
2021-12-31 13:35:11 -0500

    completed problem 33

--- python/pr33.py
+++ python/pr33.py
@@ -3,10 +3,10 @@ from typing import *
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         pivot, pivot_idx = self.find_pivot(nums)
-        print(f"{pivot}:{pivot_idx}")
+        # print(f"pivot: {pivot}:{pivot_idx}")
         low = pivot_idx if pivot_idx >= 0 else 0
         high = pivot_idx - 1 if pivot_idx >= 0 else len(nums) - 1
-        print(f"{low}:{high}")
+
         # binary search through rotated array for target
         while True:
             # if current sublist we are searching contains the rotated
@@ -30,13 +30,19 @@ class Solution:
                 # rotated array we need to take to get to mid
                 mid = (mid - low) - (len(nums) - low)
 
+            #print(f"{low}:{mid}:{high}")
+            #print(f"{adj_low}:{mid}:{adj_high}(adj)")
+
             if target == nums[mid]:
                 return mid
             else:
                 if target < nums[mid]:
-                    high = mid - 1
+                    high = mid #- 1
                 elif target > nums[mid]:
                     low = mid + 1
+
+            if adj_high == adj_low:
+                break
 
         return -1
 
@@ -44,11 +50,15 @@ class Solution:
         low = 0
         high = len(nums) - 1
 
+        # is array unrotated?
+        if nums[high] > nums[low]:
+            return -1,-1
+
         while low <= high:
             mid = low + (high - low) // 2
 
             if nums[low] < nums[mid]:
-                low = mid # mid + 1?
+                low = mid #+ 1
             else:
                 if mid == 0:
                     if len(nums) > 1 and nums[mid + 1] < nums[mid]:
@@ -58,8 +68,13 @@ class Solution:
 
                 if nums[mid] < nums[mid - 1]:
                     return len(nums) - mid + 1, mid
+                elif mid < len(nums) and nums[mid] > nums[mid + 1]:
+                    return len(nums) - mid, mid + 1
                 else:
                     high = mid
+
+            if low == high:
+                break
 
         return -1,-1
 
@@ -68,9 +83,7 @@ nums = [5,6,1,2,3,4]
 # nums = [3,1]
 # nums = [4,5,6,7,0,1,2]
 # nums = [1]
-target = 7
-# 4,3,0
-# 4,0,5
-# 5,0,6
-# 6,0,6
+# nums = [1,3,5]
+nums = [3,5,1]
+target = 1
 print(sol.search(nums, target))
