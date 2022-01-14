50238744f2e3f962c13d17651a6027245d75a982
Sean McShane <svsean416@yahoo.com>
2022-01-13 22:32:48 -0500

    completed problem 452

--- python/pr452.py
+++ python/pr452.py
@@ -2,32 +2,13 @@ from typing import List
 
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        # dict {x:set(i)}
-        # set of popped balloons
-        # for each balloon in dict
-        # if every balloon is unpopped, add to popped and increase counter
-        # if atleast one baloon is popped, the rest are also, add to popped
-        
-        d = {}
-        p = set()
-        arrows = 0
-        for i, point in enumerate(points):
-            for k in range(point[0], point[1]+1):
-                if d.get(k):
-                    d[k].add(i)
-                else:
-                    d[k] = {i}
-                    
-        for x, bals in d.items():
-            popped = False
-            for bal in bals:
-                if bal in p:
-                    popped = True
-                else:
-                    p.add(bal)
-                    
-            if not popped:
+        points.sort(key=lambda p: p[1])
+
+        arrow, arrows = 0, 0
+        for start,end in points:
+            if arrows == 0 or start > arrow:
                 arrows += 1
+                arrow = end
                 
         return arrows
 
